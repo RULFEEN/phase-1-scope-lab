65128c4869fbabcb12eecda8787a001fb7a0a43f
Victor Mayi <victormay.i.797@gmail.com>
2022-08-20 13:24:06 +0300

    commit tree 963d347267ea1e358834ab88f2276213e44e1358

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+upperCaseCustomerName();
+console.log(upperCaseCustomerName());
+var customerName = upperCaseCustomerName();
+console.log(customerName);
+
+function setBestCustomer(name){
+    var bestCustomer = name;
+    return bestCustomer;
+}
+var bestCustomer = setBestCustomer();
+console.log(bestCustomer);
+var bestCustomer = setBestCustomer('not bob');
+console.log(bestCustomer);
+
+function overwriteBestCustomer(name){
+    var bestCustomer = name;
+    return bestCustomer;
+}
+var bestCustomer = overwriteBestCustomer('maybe bob');
+console.log(bestCustomer);
+
+const leastFavoriteCustomer = 'job';
+
+function changeLeastFavoriteCustomer(name){
+    leastFavoriteCustomer = name;
+    return leastFavoriteCustomer;
+}
+changeLeastFavoriteCustomer('tom');
+console.log(changeLeastFavoriteCustomer('tom'));
